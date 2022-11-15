55496e37b8723faa7837f63e5cb9021f66a08f51
GuillemTio <gtio@edu.tecnocampus.cat>
2022-11-15 18:41:27 +0100

    main

--- main.lua
+++ main.lua
@@ -1,10 +1,7 @@
 Player = Player or require "src/Player"
-GrapplingHook = GrapplingHook or require "src/GrapplingHook"
 Camera = Camera or require"src/Camera"
 EnemyGoblin = EnemyGoblin or require"src/EnemyGoblin"
-EnemyEyes = EnemyEyes or require"src/EnemyEyes"
 EnemySkeleton = EnemySkeleton or require"src/EnemySkeleton"
-BossMushroom = BossMushroom or require "src/BossMushroom"
 HUD = HUD or require"src/HUD"
 StartMenu = StartMenu or require"src/StartMenu"
 PlayerMirror = PlayerMirror or require"src/PlayerMirror"
@@ -23,10 +20,10 @@ function love.load()
     StartMenu:new()
   else
 
-  backgroundMusic = love.audio.newSource("src/music/BigPoppa.wav","static")
-  backgroundMusic:setVolume(0.2)
-  backgroundMusic:setLooping(true)
-  backgroundMusic:play()
+  --backgroundMusic = love.audio.newSource("src/music/BigPoppa.wav","static")
+  --backgroundMusic:setVolume(0.2)
+  --backgroundMusic:setLooping(true)
+  --backgroundMusic:play()
 
   Map = STI("src/map/Map1.lua", { "box2d" })
   World = love.physics.newWorld(0, 0) -- takes x and y velocity for the World, for example to create gravity
@@ -36,14 +33,12 @@ function love.load()
   Map.layers.entity.visible = false
   
   MapWidth = Map.layers.ground.width * 24
-  background = love.graphics.newImage("src/textures/background/background_layer_1.png") -- this is for our future background
-  background2 = love.graphics.newImage("src/textures/background/background_layer_2.png")
-  background3 = love.graphics.newImage("src/textures/background/background_layer_3.png")
+  --background = love.graphics.newImage("src/textures/background/background_layer_1.png") -- this is for our future background
+  --background2 = love.graphics.newImage("src/textures/background/background_layer_2.png")
+  --background3 = love.graphics.newImage("src/textures/background/background_layer_3.png")
 
   EnemyGoblin.loadAssets()
-  EnemyEyes.loadAssets()
   EnemySkeleton.loadAssets()
-  BossMushroom.loadAssets()
   PlayerMirror:new()  
   Player:new()
   HUD:load()
@@ -67,9 +62,7 @@ function love.update(dt)
   Player:update(dt)
   PlayerMirror:update(dt)  
   EnemyGoblin.updateAll(dt)
-  EnemyEyes.updateAll(dt)
   EnemySkeleton.updateAll(dt)
-  BossMushroom.updateAll(dt)
   Camera:setPosition(Player.x, 0)
   HUD:update(dt)
   end
@@ -83,9 +76,9 @@ function love.draw()
     StartMenu:draw()
   else
 
-  love.graphics.draw(background, 0, 0, 0, 5, 5) -- this is for our future background, it should be always before the map
-  love.graphics.draw(background2, 0, 0, 0, 5, 5)
-  love.graphics.draw(background3, 0, 0, 0, 5, 5)
+  --love.graphics.draw(background, 0, 0, 0, 5, 5) -- this is for our future background, it should be always before the map
+  --love.graphics.draw(background2, 0, 0, 0, 5, 5)
+  --love.graphics.draw(background3, 0, 0, 0, 5, 5)
 
   Map:draw(-Camera.x, -Camera.y, Camera.scale, Camera.scale)
 
@@ -95,9 +88,7 @@ function love.draw()
   Player:draw()
   PlayerMirror:draw()  
   EnemyGoblin.drawAll()
-  EnemyEyes.drawAll()
   EnemySkeleton.drawAll()
-  BossMushroom.drawAll()
 
   Camera:clear()
   HUD:draw()
@@ -118,9 +109,7 @@ end
 function beginContact(a, b, collision)
   if gameStarted then
   EnemyGoblin.beginContact(a, b, collision)
-  EnemyEyes.beginContact(a, b, collision)
   EnemySkeleton.beginContact(a, b, collision)
-  BossMushroom.beginContact(a, b, collision)
   if a == Player.physics.fixture or b == Player.physics.fixture then
     Player:beginContact(a, b, collision)  
   end
@@ -144,14 +133,8 @@ function spawnEntities()
     if v.type == "enemyGoblin" then
       EnemyGoblin:new(v.x + v.width / 2, v.y + v.height / 2)
     end
-    if v.type == "enemyEyes" then
-      EnemyEyes:new(v.x + v.width / 2, v.y + v.height / 2)
-    end
     if v.type == "enemySkeleton" then
       EnemySkeleton:new(v.x + v.width / 2, v.y + v.height / 2)
-    end
-    if v.type == "bossMushroom" then
-      BossMushroom:new(v.x + v.width / 2, v.y + v.height / 2)
     end
   end
 end
