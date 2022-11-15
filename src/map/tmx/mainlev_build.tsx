e6cf0f393954ed65205338d24cfa8ec00b7b3d21
JoelJolis <jjolis@edu.tecnocampus.cat>
2022-11-15 19:25:43 +0100

    Map1

--- src/map/tmx/mainlev_build.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="mainlev_build" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="PixelFantasy_Caves_1.0/mainlev_build.png" width="1024" height="1024"/>
-</tileset>
